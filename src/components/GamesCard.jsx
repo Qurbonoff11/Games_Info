a5042effcd3caaa52fb2984a263df160e169373e
Qurbonoff11 <jonibekqurbonov570@gmail.com>
2025-09-14 09:16:53 +0500

    add api

--- src/components/GamesCard.jsx
+++ src/components/GamesCard.jsx
@@ -1,36 +1,54 @@
-import data from "../data/db";
+import { useEffect, useState } from "react";
+// import data from "../data/db";
 
-const { db } = data;
+// const { db } = data;
 
 const GamesCard = () => {
+  const [db, setDb] = useState([]);
+
+  const KEY = "ecc44a0015a645d8933c0cd1069cf024";
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const req = await fetch(`https://api.rawg.io/api/games?key=${KEY}`);
+        const data = await req.json();
+        setDb(data.results);
+        console.log(data.results);
+      } catch (error) {
+        console.error("Failed to fetch games:", error);
+      }
+    };
+    fetchMovies();
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center">
       <h2 className="text-3xl uppercase text-white font-bold">All Games</h2>
       <div className="grid grid-cols-5 gap-8 py-10">
         {db.map((item) => {
+          const { id, name, rating, released, background_image, genres, platforms } = item;
           return (
-            <div
-              className="bg-slate-700 overflow-hidden rounded-2xl"
-              key={item.id}
-            >
+            <div className="bg-slate-700 overflow-hidden rounded-2xl h-80 flex flex-col" key={id}>
               <img
                 className="w-full h-40 object-cover"
-                src={item.img}
-                alt={item.title}
+                src={background_image}
+                alt={name}
               />
-              <div className="py-2 px-4 flex flex-col justify-center gap-2">
-                <h3 className="text-white text-sm">{item.title}</h3>
-                <p className="desc text-slate-400 text-xs">
-                  {item.description}
+              <div className="py-2 px-4 flex-1 flex flex-col justify-between gap-2">
+                <h3 className="text-white text-sm">{name}</h3>
+                <p className="text-slate-200 font-ubuntu text-xs">Released: <span>{released.split("-").reverse().join(".")}</span></p>
+                <p className="text-slate-400 text-xs">
+                  {genres.map((genre) => genre.name).join(", ")}
                 </p>
-                <div className="flex items-center justify-between mt-4">
+                <span className="desc text-slate-400 text-xs">
+                  {
+                    platforms.map((platform) => platform.platform.name).join(", ")
+                  }
+                </span>
                   <span className="text-white text-xs">
-                    {item.price === "Free" ? "Free" : `$${item.price}`}
+                    {`Rating: ${rating}`}
                   </span>
-                  <button className="bg-blue-400 text-[14px] text-white font-bold uppercase py-[2px] px-4 rounded-lg cursor-pointer">
-                    Info
-                  </button>
-                </div>
               </div>
             </div>
           );
